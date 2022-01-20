0cf28ef7e661f33822de7ec11447a25a426e781c
Amad <amadou.traoredev@gmail.com>
2022-01-20 18:37:11 +0100

    fin du projet

--- code.js
+++ code.js
@@ -2,7 +2,9 @@ let btnAfr = document.getElementById("afrique");
 let btnAmq = document.getElementById("ameriques");
 let btnAsie = document.getElementById("asie");
 let btnEur = document.getElementById("europe");
+let btnOce = document.getElementById("oceanie");
 let affichePays = document.querySelector("div");
+
 
 
 function showCountryByContinent(continent){
@@ -10,21 +12,25 @@ function showCountryByContinent(continent){
     let req = new XMLHttpRequest
     req.open("get", 'https://restcountries.com/v3.1/lang/french');
     req.responseType = 'json';
+    
+    req.send();
+    req.onload = () => {
+        if (req.readyState == XMLHttpRequest.DONE){
+            if (req.status == 200) {
+                
+                let world = req.response;
+                let drapeau;
+                for (const countries of world) {
+                    if (countries.region.includes(continent)) {
+                        drapeau = countries.flags.png;
+                        let divP = document.createElement('div');
+                        divP.classList.add('divPays')
+                        affichePays.appendChild(divP);
 
-     req.send();
-     req.onload = () => {
-         if (req.readyState == XMLHttpRequest.DONE){
-             if (req.status == 200) {
-
-                 let world = req.response;
-                 let drapeau;
-                 for (const countries of world) {
-                     if (countries.region.includes(continent)) {
-                         affichePays.innerHTML += "<p>"+"Nom : "+countries.name.common +"</p>";
-                         affichePays.innerHTML += "<p>"+"Capital : "+countries.capital + "</p>";
-                         drapeau = countries.flags.png;
-                         affichePays.innerHTML += '<img src=' + drapeau +' alt="drapeau pays">'+"<br>"+"<br>";
-                         console.log(drapeau);
+                        // document.affichePays.appendChild("divP")
+                         divP.innerHTML += "<p>"+"Name : "+countries.name.common +"<br>"+"Nom : "+countries.name.nativeName.fra.common + "</p>";
+                         divP.innerHTML += "<p>"+"Capital : "+countries.capital + "</p>";
+                         divP.innerHTML += '<img src=' + drapeau +' alt="drapeau pays">'+"<br>"+"<br>";
 
                 }
             }
@@ -44,4 +50,7 @@ btnAsie.addEventListener('click',()=> {
 });
 btnEur.addEventListener('click',()=> {
     showCountryByContinent('Europe')
+});
+btnOce.addEventListener('click',()=> {
+    showCountryByContinent('Oceania')
 });
